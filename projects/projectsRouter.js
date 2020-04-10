80ac2a55d4dada315af1423ac1397865df965ad6
Michael-B1764 <mikebailar@gmail.com>
2020-04-10 09:27:23 -0700

    Projects router up to MVP with correct validation and errors.

--- projects/projectsRouter.js
+++ projects/projectsRouter.js
@@ -7,16 +7,19 @@ const router = express.Router()
 router.get('/', (req, res) => {
     Projects.get()
     .then(projects => {
-        res.status(200).json(projects)
+        if (projects.length > 0) {
+            res.status(200).json(projects)
+        } else {
+            res.status(404).json({ message: "project not found" })
+        }
     })
     .catch(error => {
-        console.log(error)
+        res.status(500).json({message: "Error occured retrieving projects"})
     })
 })
 
 router.get('/:id', (req, res) => {
     const id = req.params.id
-    if (req) {
         Projects.get(id)
             .then(project => {
                 if (project) {
@@ -26,55 +29,65 @@ router.get('/:id', (req, res) => {
                 }
             })
             .catch(error => {
-                console.log(error)
+                res.status(500).json({message: "Error occured retrieving project"})
             })
-    } else {
-        console.log(req)
-        res.status(404).json({ message: "project not found" })
-    }
 })
 
 router.post('/', (req, res) => {
-    Projects.insert(req.body)
-    .then(project => {
-        res.status(200).json({project})
-    })
-    .catch(error => {
-        console.log(error)
-    })
+    if (req.body.name && req.body.description) {
+        Projects.insert(req.body)
+        .then(project => {
+            res.status(201).json({project})
+        })
+        
+    } else {
+        res.status(400).json({message: "please input a project name and description"})
+    }
 })
 
 router.put('/:id', (req, res) => {
     const id = req.params.id
     const changes = req.body
-    Projects.update(id, changes)
-    .then(project => {
-        res.status(200).json({project})
-    })
-    .catch(error => {
-        console.log(error)
-    })
+        Projects.update(id, changes)
+        .then(projct => {
+            if(!project) {
+                res.status(404).json({ message: "project not found" })
+            } else {
+                res.status(202).json({message: "project successfuly updated"})
+            }
+        })
+        .catch(error => {
+            res.status(500).json({message: "Error occured updating project"})
+        })
 })
 
 router.delete('/:id', (req, res) => {
     const id = req.params.id
     Projects.remove(id)
         .then(project => {
-            console.log(project)
-            res.status(200).json({ message: "1 project successfully deleted" })
+            if (!project) {
+                res.status(404).json({ message: "project not found" })
+            } else {
+            res.status(202).json({ message: "1 project successfully deleted" })
+            }
         })
         .catch(error => {
-            console.log(error)
+            res.status(500).json({message: "Error occured updating project"})
         })
 })
+
 router.get('/:id/actions', (req, res) => {
     const id = req.params.id
     Projects.getProjectActions(id)
     .then(actions => {
-        res.status(200).json({actions})
+        if (actions.length > 0) {
+            res.status(200).json(actions)
+        } else {
+            res.status(404).json({ message: "project not found" })
+        }
     })
     .catch(error => {
-        console.log(error)
+        res.status(500).json({message: "Error occured updating project"})
     })
 })
 
